6e3bf16ec97711f6cb230ce7d55176d5507bc051
Seokhee Park <seokhee_park@nexcubecorp.com>
2025-11-17 17:10:28 +0900

    feat: 서버 정상 동작 확인

---
+++ acceptance/steps/health.steps.js
@@ -0,0 +1,20 @@
+import {When, Then} from "@cucumber/cucumber";
+import request from "supertest";
+import app from "../../src/app.js";
+
+let response;
+
+When('클라이언트가 헬스 체크 엔드포인트에 GET 요청을 보낸다', async function() {
+    response = await request(app)
+        .get('/health')
+        .set('Accept', 'application/json')
+});
+
+Then('응답의 상태코드는 {int}이어야 한다', function (expectedStatusCode) {
+    if (response.statusCode !== expectedStatusCode) {
+        throw new Error(`Expected status code ${expectedStatusCode}, but got ${response.statusCode}`);
+    }
+});
+Then('응답의 {string} 필드는 {string} 이어야 한다', function (field, expectedValue) {
+
+});
---
+++ cucumber.mjs
@@ -0,0 +1,6 @@
+export default {
+    paths: ['acceptance/features/**/*.feature'],
+    import: ['acceptance/steps/**/*.js'],
+    format: ['progress'],
+    publishQuiet: true,
+};
