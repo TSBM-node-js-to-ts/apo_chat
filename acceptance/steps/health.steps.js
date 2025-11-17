56de668600636748c56cb6189afb41c14b41bfdb
Seokhee Park <seokhee_park@nexcubecorp.com>
2025-11-17 18:13:23 +0900

    setting: db 연동

--- acceptance/steps/health.steps.js
+++ acceptance/steps/health.steps.js
@@ -15,6 +15,13 @@ Then('응답의 상태코드는 {int}이어야 한다', function (expectedStatus
         throw new Error(`Expected status code ${expectedStatusCode}, but got ${response.statusCode}`);
     }
 });
-Then('응답의 {string} 필드는 {string} 이어야 한다', function (field, expectedValue) {
 
+Then('응답의 {string} 필드는 {string} 이어야 한다', function (field, expectedValue) {
+    if (!Object.prototype.hasOwnProperty.call(response.body, field)) {
+        throw new Error(`Expected field ${field} not found in response bod, got: ${JSON.stringify(response.body)}`);
+    }
+    const actual = String(response.body[field]);
+    if (actual !== expectedValue) {
+        throw new Error(`Expected field ${field} to have value ${expectedValue}, but got ${actual}`);
+    }
 });
