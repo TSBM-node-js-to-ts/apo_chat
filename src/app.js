52d140db8e659aa4a4d595797031b458e67afb14
StoneHee99 <kiit0901@gmail.com>
2025-11-17 20:12:43 +0900

    fix: 테스트 실행 오류 수정

--- src/app.js
+++ src/app.js
@@ -1,11 +1,8 @@
 import express from 'express';
 import healthRouter from './routes/health.route.js';
-import { sequelize } from "./db/sequelize.js";
 
 const app = express();
 app.use(express.json());
-
-await sequelize.sync({ alter: true });
 
 app.use('/health', healthRouter);
 
--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import 'dotenv/config';
 import app from './app.js';
+import {sequelize} from "./db/sequelize.js";
 
 const PORT = process.env.PORT;
+
+await sequelize.sync({ alter: true });
 
 app.listen(PORT, () => {
     console.log(`🚀 Server listening on http://localhost:${PORT}`)
